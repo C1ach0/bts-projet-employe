847f9652ee0967107255b2d43d2a9ca255780db9
Max <c1ach83500@gmail.com>
2026-02-09 08:12:17 +0100

    first commit

---
+++ src/main/java/fr/rosydev/projectemploye/ProjectemployeApplication.java
@@ -0,0 +1,13 @@
+package fr.rosydev.projectemploye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectemployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectemployeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/rosydev/projectemploye/ProjectemployeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rosydev.projectemploye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectemployeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
